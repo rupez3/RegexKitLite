f992213bccd183644278bc71fe0a09a1edb47335
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2017-03-15 11:15:02 -0400

    first commit

---
+++ RegexKitLiteSwift/RegexKitLiteSwift.h
@@ -0,0 +1,20 @@
+//
+//  RegexKitLiteSwift.h
+//  RegexKitLiteSwift
+//
+//  Created by Chhetri, Rupesh on 3/15/17.
+//  Copyright © 2017 RupeshChhetri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RegexKitLite.h"
+
+//! Project version number for RegexKitLiteSwift.
+FOUNDATION_EXPORT double RegexKitLiteSwiftVersionNumber;
+
+//! Project version string for RegexKitLiteSwift.
+FOUNDATION_EXPORT const unsigned char RegexKitLiteSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegexKitLiteSwift/PublicHeader.h>
+
+
